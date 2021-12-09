1909871c3a39744d73214887120396d7bc2903f2
Sasha <Alexandr>
2021-12-09 15:50:34 +0300

    Class FoodComparator

---
+++ src/com/company/FoodComparator.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Comparator;
+
+public class FoodComparator implements  Comparator<Food>
+{
+
+    public int compare(Food arg0, Food arg1) {
+        if (arg0==null) return 1;
+
+        if (arg1==null) return -1;
+
+        return arg0.getName().compareTo(arg1.getName());
+    }
+
+
+}
